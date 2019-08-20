000eda459e1d0020d2312e741c1fa79a0f8328c9
wangqiming1001 <52316484+wangqiming1001@users.noreply.github.com>
2019-08-20 23:17:58 +0800

    Update main-spec.js

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -22,3 +22,23 @@ it('should generate array with table items',() =>{
         expect(generateArry[1][0],'2*3=6');
         expect(generateArry[1][1],'3*3=9');
     });
+
+it ('should generate result string', () => {
+     //given
+     const multiplyArray = [];
+     multiplyArray[0] = [];
+     multiplyArray[0][0] = "2*2=4";
+     //when
+     const result = printMultiplyTable(multiplyArray);
+     //then
+     expect(result).toBe("2*2=4 \n");
+});
+
+it ('should print multiplyTable', () => {
+     //given
+     const firstNumber = 2;
+     const secondNumber = 3;
+     //when
+     const result = multiplyTable(firstNumber,secondNumber);
+     //then
+     expect(result).toBe("2*2=4 \n2*3=6 3*3=9 \n");
